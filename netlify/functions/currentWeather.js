067e465722d93ec57ab6990162ab120cb5c331f9
ivailo-iliev <ivailo.iliev@gmail.com>
2023-07-13 00:26:38 +0300

    .

--- netlify/functions/currentWeather.js
+++ netlify/functions/currentWeather.js
@@ -1,10 +1,18 @@
 const fetch = require('node-fetch')
 
 exports.handler = async function () {
-    const currentWeatherApi = "https://dataservice.accuweather.com/currentconditions/v1/51097?apikey=8GM4gMnnGKurUdAFMBNrFobGX1XnG1kG&language=bg-bg&details=true";
-    
-  const fetchCurrentWeather = await fetch(currentWeatherApi)
-  const currentWeatherData = await fetchCurrentWeather.json()
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", "Bearer patK67o9PJI2V7wJI.58ff50c61d33b346880bcd7eaf6bb93ad8882303b0c7a47387e731f5dee6cf5d");
+
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+
+  const airtableApi = "https://api.airtable.com/v0/appCu46edF9GYofCL/tblwhM5yFy4N4XpKk/viwdROa0LNFQOyJSi/recUF1J84zlIwn0Y8";
+  const fetchCurrentWeather = await fetch(airtableApi, requestOptions);
+  const currentWeatherData = await fetchCurrentWeather.json();
 
   return {
     statusCode: 200,
--- netlify/functions/todayForecast.js
+++ netlify/functions/todayForecast.js
@@ -1,10 +1,18 @@
 const fetch = require('node-fetch')
 
 exports.handler = async function () {
-  const todayForecastApi = "https://dataservice.accuweather.com/forecasts/v1/daily/1day/51097?apikey=8GM4gMnnGKurUdAFMBNrFobGX1XnG1kG&language=bg-bg&details=true&metric=true";
-    
-  const fetchTodayForecast = await fetch(todayForecastApi)
-  const todayForecastData = await fetchTodayForecast.json()
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", "Bearer patK67o9PJI2V7wJI.58ff50c61d33b346880bcd7eaf6bb93ad8882303b0c7a47387e731f5dee6cf5d");
+
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+
+  const airtableApi = "https://api.airtable.com/v0/appCu46edF9GYofCL/tblwhM5yFy4N4XpKk/viwdROa0LNFQOyJSi/recUF1J84zlIwn0Y8";
+  const fetchTodayForecast = await fetch(airtableApi, requestOptions);
+  const todayForecastData = await fetchTodayForecast.json();
 
   return {
     statusCode: 200,
