b2f1914a8a84c614ca68733aa44bcd9501063c35
ivailo-iliev <ivailo.iliev@gmail.com>
2023-07-12 23:42:04 +0300

    Update scheduleWeather.js

--- netlify/functions/scheduleWeather.js
+++ netlify/functions/scheduleWeather.js
@@ -2,20 +2,24 @@ const fetch = require('node-fetch');
 
 exports.handler = async function (event, context) {
   const weatherData =
-    ({
-      "id": "recLxkU2dyt16sIwf",
-      "fields": {
-        "Name": "todaysForecast",
-        "Value": "123"
+  {
+    "records": [
+      {
+        "id": "recLxkU2dyt16sIwf",
+        "fields": {
+          "Name": "todaysForecast",
+          "Value": "123"
+        }
+      },
+      {
+        "id": "recUF1J84zlIwn0Y8",
+        "fields": {
+          "Name": "currentWeather",
+          "Value": "123"
+        }
       }
-    },
-    {
-      "id": "recUF1J84zlIwn0Y8",
-      "fields": {
-        "Name": "currentWeather",
-        "Value": "123"
-      }
-    });
+    ]
+  };
 
   var myHeaders = new Headers();
   myHeaders.append("Authorization", "Bearer patK67o9PJI2V7wJI.58ff50c61d33b346880bcd7eaf6bb93ad8882303b0c7a47387e731f5dee6cf5d");
