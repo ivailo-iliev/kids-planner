eefc49b275a9103a60829f5b6543c628dd893855
ivailo-iliev <ivailo.iliev@gmail.com>
2023-07-12 23:37:16 +0300

    Update scheduleWeather.js

--- netlify/functions/scheduleWeather.js
+++ netlify/functions/scheduleWeather.js
@@ -1,6 +1,22 @@
 const fetch = require('node-fetch');
 
-exports.handler = async function(event, context) {
+exports.handler = async function (event, context) {
+  const weatherData =
+    ({
+      "id": "recLxkU2dyt16sIwf",
+      "fields": {
+        "Name": "todaysForecast",
+        "Value": "123"
+      }
+    },
+    {
+      "id": "recUF1J84zlIwn0Y8",
+      "fields": {
+        "Name": "currentWeather",
+        "Value": "123"
+      }
+    });
+
   var myHeaders = new Headers();
   myHeaders.append("Authorization", "Bearer patK67o9PJI2V7wJI.58ff50c61d33b346880bcd7eaf6bb93ad8882303b0c7a47387e731f5dee6cf5d");
   myHeaders.append("Content-Type", "application/json");
@@ -12,23 +28,8 @@ exports.handler = async function(event, context) {
   };
   const airtableApi = "https://api.airtable.com/v0/appCu46edF9GYofCL/current-weather";
 
-  const weatherData = 
-    ({
-      "id": "recLxkU2dyt16sIwf",
-      "fields": {
-        "Name": "todaysForecast",
-        "Value": new Date()
-      }
-    },
-    {
-      "id": "recUF1J84zlIwn0Y8",
-      "fields": {
-        "Name": "currentWeather",
-        "Value": new Date()
-      }
-    });
-    const updateWeather = await fetch(airtableApi, requestOptions);
-    console.log("executed");
+  const updateWeather = await fetch(airtableApi, requestOptions);
+  console.log("executed");
 
   return {
     statusCode: 200
