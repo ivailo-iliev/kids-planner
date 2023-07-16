36c3ea11f555e5db1c332aba63bfdb584cc61568
ivailo-iliev <ivailo.iliev@gmail.com>
2023-07-16 14:41:23 +0300

    Update scheduleWeather.js

--- netlify/functions/scheduleWeather.js
+++ netlify/functions/scheduleWeather.js
@@ -1,15 +1,36 @@
 const fetch = require('node-fetch');
 
+async function fetchData(apiUrl, headers) {
+  const response = await fetch(apiUrl, headers);
+  const data = await response.json();
+  return data;
+}
+
+async function patchData(apiUrl, headers, body) {
+  const requestOptions = {
+    method: 'PATCH',
+    headers: headers,
+    redirect: 'follow',
+    body: JSON.stringify(body)
+  };
+
+  const response = await fetch(apiUrl, requestOptions);
+  const responseData = await response.json();
+  return responseData;
+}
+
 exports.handler = async function (event, context) {
   const currentWeatherApi = "https://dataservice.accuweather.com/currentconditions/v1/51097?apikey=8GM4gMnnGKurUdAFMBNrFobGX1XnG1kG&language=bg-bg&details=true";
-    
-  const fetchCurrentWeather = await fetch(currentWeatherApi)
-  const currentWeatherData = await fetchCurrentWeather.json()
-
   const todayForecastApi = "https://dataservice.accuweather.com/forecasts/v1/daily/1day/51097?apikey=8GM4gMnnGKurUdAFMBNrFobGX1XnG1kG&language=bg-bg&details=true&metric=true";
-    
-  const fetchTodayForecast = await fetch(todayForecastApi);
-  const todayForecastData = await fetchTodayForecast.json();
+
+  const forecastRequestHeaders = {
+    headers: {
+      "Accept-Encoding": "gzip,deflate"
+    }
+  };
+
+  const currentWeatherData = await fetchData(currentWeatherApi, forecastRequestHeaders);
+  const todayForecastData = await fetchData(todayForecastApi, forecastRequestHeaders);
 
   const weatherDataCache =
   {
@@ -31,19 +52,14 @@ exports.handler = async function (event, context) {
     ]
   };
 
-  var myHeaders = new Headers();
-  myHeaders.append("Authorization", "Bearer patK67o9PJI2V7wJI.58ff50c61d33b346880bcd7eaf6bb93ad8882303b0c7a47387e731f5dee6cf5d");
-  myHeaders.append("Content-Type", "application/json");
-  var requestOptions = {
-    method: 'PATCH',
-    headers: myHeaders,
-    redirect: 'follow',
-    body: JSON.stringify(weatherDataCache)
+  const airtableHeaders = {
+    "Authorization": "Bearer patK67o9PJI2V7wJI.58ff50c61d33b346880bcd7eaf6bb93ad8882303b0c7a47387e731f5dee6cf5d",
+    "Content-Type": "application/json"
   };
+
   const airtableApi = "https://api.airtable.com/v0/appCu46edF9GYofCL/current-weather";
-  const updateWeather = await fetch(airtableApi, requestOptions);
-  const updateResponse = await updateWeather.json();
-  console.log(updateResponse);
+  const updateResponse = await patchData(airtableApi, { headers: airtableHeaders }, weatherDataCache);
+
   return {
     statusCode: 200,
     body: JSON.stringify(updateResponse)
