d5607c6103145d36f4ccabe6587781b468742978
ivailo-iliev <ivailo.iliev@gmail.com>
2023-07-16 11:59:54 +0300

    Update userDetails.js

--- netlify/functions/userDetails.js
+++ netlify/functions/userDetails.js
@@ -1,8 +1,9 @@
 const fetch = require('node-fetch')
 
-exports.handler = async function (event,context) {
-   console.log(event.queryStringParameters);
-   var myHeaders = new Headers();
+exports.handler = async function (event, context) {
+  const UserId = event.queryStringParameters.id;
+  console.log(UserId);
+  var myHeaders = new Headers();
   myHeaders.append("Authorization", "Bearer patK67o9PJI2V7wJI.58ff50c61d33b346880bcd7eaf6bb93ad8882303b0c7a47387e731f5dee6cf5d");
 
   var requestOptions = {
@@ -11,12 +12,12 @@ exports.handler = async function (event,context) {
     redirect: 'follow'
   };
 
-  const airtableApi = "https://api.airtable.com/v0/appCu46edF9GYofCL/week?filterByFormula=UserID%3D%22reckRn41ZCJm4333N%22&view=even";
+  const airtableApi = "https://api.airtable.com/v0/appCu46edF9GYofCL/week?filterByFormula=UserID%3D%22"+UserId+"%22&view=even";
   const fetchCalendar = await fetch(airtableApi, requestOptions);
   const calendarData = await fetchCalendar.json();
 
   return {
     statusCode: 200,
-    body: JSON.stringify(event.queryStringParameters)
+    body: JSON.stringify(calendarData)
   }
 }
